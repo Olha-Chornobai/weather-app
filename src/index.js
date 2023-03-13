ee27d043563e44422a35d49e709f6f73b5f4358d
Olha-Chornobai <127105479+Olha-Chornobai@users.noreply.github.com>
2023-03-13 15:41:41 +0000

    Added description

--- src/index.js
+++ src/index.js
@@ -65,6 +65,7 @@ function displayWeather(response) {
   document.querySelector("#tempa").innerHTML = Math.round(
     response.data.main.temp
   );
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
