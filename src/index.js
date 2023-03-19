3d611704ce0c68e631527990de6cc7f92e8318f8
Olha-Chornobai <127105479+Olha-Chornobai@users.noreply.github.com>
2023-03-19 15:24:31 +0000

    Update JS file

--- src/index.js
+++ src/index.js
@@ -21,35 +21,36 @@ if (minutes < 10) {
   currentDate.innerHTML = `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
+// function displayForecast() {
+//   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+//   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
 
-  let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-      <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
-        <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
-        </div>
-      </div>
-  `;
-  });
+//   let forecastHTML = `<div class="row">`;
+//   days.forEach(function (day) {
+//     forecastHTML =
+//       forecastHTML +
+//       `
+//       <div class="col-2">
+//         <div class="weather-forecast-date">${day}</div>
+//         <img
+//           src="http://openweathermap.org/img/wn/50d@2x.png"
+//           alt=""
+//           width="42"
+//         />
+//         <div class="weather-forecast-temperatures">
+//           <span class="weather-forecast-temperature-max"> 18° </span>
+//           <span class="weather-forecast-temperature-min"> 12° </span>
+//         </div>
+//       </div>
+//   `;
+//   });
 
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
-}
+//   forecastHTML = forecastHTML + `</div>`;
+//   forecastElement.innerHTML = forecastHTML;
+//   console.log(forecastHTML);
+// }
+
 
 function search(event) {
   event.preventDefault();
@@ -106,6 +107,8 @@ function displayWeather(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#icon").setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+ 
 }
 
 
@@ -142,4 +145,4 @@ currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("London");
 
-displayForecast();
+// displayForecast();
